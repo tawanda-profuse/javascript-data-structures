e49386f4ea1d861289da9af2156a2ebcb6515a17
Tawanda Msengezi <xrthemc@gmail.com>
2022-05-21 17:45:56 +0200

    Created a class for the list

--- doublyLinkedLists.js
+++ doublyLinkedLists.js
@@ -13,3 +13,57 @@ class Node{
         this.prev = null;
     }
 }
+
+// We create a class for the list
+class DoublyLinkedList {
+    // The list has three properties, the head, the tail and the list size
+    constructor(){
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+    // The push method takes a value as parameter and assigns it as the tail of the list
+    push(val){
+        const newNode = new Node(val)
+        if(this.length === 0){
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.tail.next = newNode
+            newNode.prev = this.tail
+            this.tail = newNode
+        }
+        this.length++
+        return this
+    }
+    // The pop method removes the tail of the list
+    pop(){
+        if(!this.head) return undefined
+        const poppedNode = this.tail
+        if(this.length === 1){
+            this.head = null
+            this.tail = null
+        } else {
+            this.tail = poppedNode.prev
+            this.tail.next = null
+            poppedNode.prev = null
+        }
+        this.length--
+        return poppedNode
+    }
+    // The shift method removes the head of the list
+    shift(){
+        if(this.length === 0) return undefined
+        const oldHead = this.head
+        if(this.length === 1){
+            this.head = null
+            this.tail = null
+        } else{
+            this.head = oldHead.next
+            this.head.prev = null
+            oldHead.next = null
+        }
+        this.length--
+        return oldHead
+    }
+    
