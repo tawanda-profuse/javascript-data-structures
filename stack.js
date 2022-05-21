444030852c208d068722ba5cacd415bd314527ed
Tawanda Msengezi <xrthemc@gmail.com>
2022-05-21 17:31:37 +0200

    Added the stack module

---
+++ stack.js
@@ -0,0 +1,71 @@
+/*Stacks are a data structure that store information in the form of a list. They allow only adding and removing elements under a LIFO pattern (last in, first out). In stacks, elements can't be added or removed out of order, they always have to follow the LIFO pattern.*/
+
+// Implementing a stack as a list will look like this:
+
+// We create a class for each node within the stack
+class Node{
+    // Each node has two properties, its values and a pointer that indicates the node that follows
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+// We create a class for the stack
+class Stack{
+    // The stack has three properties, the first node, the last node and the stack size
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    // The push method receives a value and adds it to the "top" of the stack
+    push(val){
+        var newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            var temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.size;
+    }
+    // The pop method eliminates the element at the "top" of the stack and returns its value
+    pop(){
+        if(!this.first) return null;
+        var temp = this.first;
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
+}
+
+const stck = new Stack;
+
+stck.push("value1");
+stck.push("value2");
+stck.push("value3");
+
+console.log(stck.first); /* 
+        Node {
+        value: 'value3',
+        next: Node { value: 'value2', next: Node { value: 'value1', next: null } }
+        }
+    */
+console.log(stck.last); // Node { value: 'value1', next: null }
+console.log(stck.size); // 3
+
+stck.push("value4");
+console.log(stck.pop()); // value4
+
+/*The big O of stack methods is the following:
+    Insertion - O(1)
+    Removal - O(1)
+    Searching - O(n)
+    Access - O(n)
+*/
