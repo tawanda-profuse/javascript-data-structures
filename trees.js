20755107c2506176edbd6b5be709cbc4bb59c5cf
Tawanda Msengezi <xrthemc@gmail.com>
2022-05-21 18:22:26 +0200

    Added the find method

--- trees.js
+++ trees.js
@@ -43,3 +43,39 @@ class BinarySearchTree {
             }
         }
     }
+
+    // The find method takes a value as parameter and iterates through the tree looking for that value
+    // If the value is found, it returns the corresponding node and if it's not, it returns undefined
+    find(value){
+        if(this.root === null) return false
+        let current = this.root,
+            found = false
+        while(current && !found){
+            if(value < current.value){
+                current = current.left
+            } else if(value > current.value){
+                current = current.right
+            } else {
+                found = true
+            }
+        }
+        if(!found) return undefined
+        return current
+    }
+    // The contains method takes a value as parameter and returns true if the value is found within the tree
+    contains(value){
+        if(this.root === null) return false
+        let current = this.root,
+            found = false
+        while(current && !found){
+            if(value < current.value){
+                current = current.left
+            } else if(value > current.value){
+                current = current.right
+            } else {
+                return true
+            }
+        }
+        return false
+    }
+}
